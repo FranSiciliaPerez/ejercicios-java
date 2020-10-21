9bdaf13bdede311214cedb087d2137c618befb0b
Francisco Javier Sicilia <franjsp23@gmail.com>
2020-10-21 11:01:53 +0200

    Adición de las acctividad 1 del tema 3

--- Primer trimestre/Tema 3/Ejercicio1.java
+++ Primer trimestre/Tema 3/Ejercicio1.java
@@ -1,1 +1,18 @@
-
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    String linea;
+    System.out.print("Introduce un número: ");
+    linea = System.console().readLine();
+    int numero1;
+    numero1 = Integer.parseInt( linea );
+    
+    System.out.print("A continuación introduce otro número: ");
+    linea = System.console().readLine();
+    int numero2;
+    numero2 = Integer.parseInt( linea );
+    int multiplicacion;
+    multiplicacion = (numero1 * numero2);
+    System.out.println("El resultado de multiplicar los dos números es: " + multiplicacion);
+  
+  }
+}
